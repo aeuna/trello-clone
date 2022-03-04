f042b51febb9045a23b2b81cef6aca66949f95d0
aeuna <4294psy@naver.com>
2022-03-04 12:26:34 +0900

    get selectors 학습

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  coinBgColor: "rgb(18, 21, 24,0.8)",
+  coinBorderColor: "#f5f6fa",
+  accentColor: "orange",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "#303952",
+  coinBgColor: "#f5f6fa",
+  coinBorderColor: "rgb(18, 21, 24,0.8)",
+  accentColor: "orange",
+};
