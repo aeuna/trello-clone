934209f7129f68b11573687b1987c46c1d58620a
aeuna <4294psy@naver.com>
2022-03-08 16:53:02 +0900

    Drag and Drop part one

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,19 +1,1 @@
 import { atom, selector } from "recoil";
-
-export const minuteState = atom({
-  // state는 오직 minutes만 있는데 selector를 사용해서 hours state까지 커버가능
-  key: "minutes",
-  default: 0,
-});
-
-export const hourSelector = selector<number>({
-  key: "hours",
-  get: ({ get }) => {
-    const minutes = get(minuteState);
-    return minutes / 60;
-  },
-  set: ({ set }, newValue) => {
-    const minutes = Number(newValue) * 60;
-    set(minuteState, minutes);
-  },
-});
