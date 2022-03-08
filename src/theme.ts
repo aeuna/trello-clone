df842e7bd97dad49724b43c0b40a7ac9682928d5
aeuna <4294psy@naver.com>
2022-03-08 17:38:33 +0900

    Styles and Placeholders

--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#f5f6fa",
-  coinBgColor: "rgb(18, 21, 24,0.8)",
-  coinBorderColor: "#f5f6fa",
-  accentColor: "orange",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "#303952",
-  coinBgColor: "#f5f6fa",
-  coinBorderColor: "rgb(18, 21, 24,0.8)",
-  accentColor: "orange",
-};
+// export const lightTheme: DefaultTheme = {
+//   bgColor: "white",
+//   textColor: "#303952",
+//   coinBgColor: "#f5f6fa",
+//   coinBorderColor: "rgb(18, 21, 24,0.8)",
+//   accentColor: "orange",
+// };
